c402d16487a856960699a7c6e6d54c1e492f94ff
Preethudg <sanjanasanjanadg59@gmail.com>
2025-03-05 23:33:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -42,7 +42,7 @@ ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002167205597 -1002308128053 -1002491660021').split()]  # Channel id for auto indexing (make sure bot is admin)
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002334874259'))  # Log channel id (make sure bot is admin)
 BIN_CHANNEL = int(environ.get('BIN_CHANNEL', '-1002294061702'))  # Bin channel id (make sure bot is admin)
-DEENDAYAL_MOVIE_UPDATE_CHANNEL = int(environ.get('DEENDAYAL_MOVIE_UPDATE_CHANNEL', '-1002188150109'))  # Notification of those who verify will be sent to your channel
+DEENDAYAL_MOVIE_UPDATE_CHANNEL = int(environ.get('DEENDAYAL_MOVIE_UPDATE_CHANNEL', '-1002349405699'))  # Notification of those who verify will be sent to your channel
 PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', '-1002420214990'))  # Premium logs channel id
 auth_channel = environ.get('AUTH_CHANNEL', '-1002472068351')  # Channel/Group ID for force sub (make sure bot is admin)
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '-1002370487322').split()]
@@ -93,10 +93,10 @@ IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', False))
 # ============================
 # Channel & Group Links Configuration
 # ============================
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/moviesrequestkannada_01')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/kannada_Movies_0527')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+9L7rvB_oh0BhNWRl')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+9L7rvB_oh0BhNWRl')
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Brand_01_01')
-DEENDAYAL_MOVIE_UPDATE_CHANNEL_LNK = environ.get('DEENDAYAL_MOVIE_UPDATE_CHANNEL_LNK', 'https://t.me/kannada_Movies_0527')
+DEENDAYAL_MOVIE_UPDATE_CHANNEL_LNK = environ.get('DEENDAYAL_MOVIE_UPDATE_CHANNEL_LNK', 'https://t.me/+dNzODJvGHIU2NTE1')
 OWNERID = int(os.environ.get('OWNERID', '2089142494'))  # Replace with the actual admin ID
 
 # ============================
